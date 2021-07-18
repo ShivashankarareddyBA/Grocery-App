ca10916f6eacdb097b4ae641993db0af92ef34b9
Jagadeesh Damarasingu <jagadeesh.damarasingu@gmail.com>
2021-07-18 19:57:12 +0530

    fixed issue with splash screen not proceeding to next screens

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -4,18 +4,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'LogInScreen.dart';
-class SplashScreen extends StatefulWidget{
+
+class SplashScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _SplashScreen();
   }
-
 }
+
 class _SplashScreen extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       backgroundColor: Color(0xff800080),
       body: Center(
@@ -28,44 +27,50 @@ class _SplashScreen extends State<SplashScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     MyImage(),
-                    Text("AJASR STORE", style: TextStyle(fontSize: 25,
-                        fontStyle: FontStyle.italic,
-                        color: Color(0xff888888)),)
+                    Text(
+                      "AJASR STORE",
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontStyle: FontStyle.italic,
+                          color: Color(0xff888888)),
+                    )
                   ],
-                )
-            ),
+                )),
             Expanded(
                 flex: 1,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     CircularProgressIndicator(),
-                    SizedBox(height: 20,),
+                    SizedBox(
+                      height: 20,
+                    ),
                     Text("Online Store For Every One",
                         style: TextStyle(
                           fontStyle: FontStyle.italic,
                           fontSize: 14,
-                          color: Color(0xffffffff),)),
+                          color: Color(0xffffffff),
+                        )),
                   ],
-                )
-            ),
+                )),
           ],
         ),
       ),
     );
   }
 
+  @override
+  void initState() {
+    super.initState();
+    navigateToLogIn();
+  }
+
   // first we need to navigate to log in screen
   void navigateToLogIn() {
-    Timer(Duration(seconds: 5), () =>
-        Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (BuildContext context) => LogInScreen())
-        ));
-    @override
-    void initState() {
-      super.initState();
-      navigateToLogIn();
-    }
+    Timer(
+        Duration(seconds: 5),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => LogInScreen())));
   }
 }
 
@@ -73,9 +78,12 @@ class MyImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    AssetImage image=new AssetImage("images/logo.jpg");
-    Image logo=new Image(image: image,width: 350,height: 350,);
-   return logo;
+    AssetImage image = new AssetImage("images/logo.jpg");
+    Image logo = new Image(
+      image: image,
+      width: 350,
+      height: 350,
+    );
+    return logo;
   }
-
 }
